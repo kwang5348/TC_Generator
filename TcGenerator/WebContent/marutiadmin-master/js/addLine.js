850268ba592e9d6101546b297dd4d37635f0923d
Ramkwang <kwang5348@gmail.com>
2020-04-01 12:53:56 +0900

    정리 중간저장

--- TcGenerator/WebContent/marutiadmin-master/js/addLine.js
+++ TcGenerator/WebContent/marutiadmin-master/js/addLine.js
@@ -3,8 +3,16 @@
 var addLine = document.getElementById('addLineBtn');
 var index = 0;
 addLine.addEventListener('click', function(){
+	alert("hi");
 	var div = document.createElement('div');
 	div.innerHTML = document.getElementById('widget-box').innerHTML;
 	div.id = index++;
 	document.getElementById('collapse-group').appendChild(div);
+})
+
+$(function(){
+   $("#addLineBtn").click(function(){
+      alert("Hello");
+      console.log("Hi");
+   });
 })
