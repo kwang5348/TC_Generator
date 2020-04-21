7d1da77a784cb110ea28fc6096c63de1cfbf9014
Ramkwang <kwang5348@gmail.com>
2020-04-21 19:49:56 +0900

    merge 충돌 해결

--- TcGenerator/WebContent/marutiadmin-master/js/main.js
+++ TcGenerator/WebContent/marutiadmin-master/js/main.js
@@ -417,39 +417,7 @@ function makeNumberString(thisObj, len){
 		}
 	}
 }
-
-<<<<<<< HEAD
-$(document).on('click', '.toggleBG', function () {
-    var toggleBG = $(this);
-    var toggleFG = $(this).find('.toggleFG');
-    var left = toggleFG.css('left');
-    if(left == '40px') {
-        toggleBG.css('background', '#CCCCCC');
-        toggleActionStart(toggleFG, 'TO_LEFT');
-    }else if(left == '0px') {
-        toggleBG.css('background', '#53FF4C');
-        toggleActionStart(toggleFG, 'TO_RIGHT');
-    }
-});
  
-// 토글 버튼 이동 모션 함수
-function toggleActionStart(toggleBtn, LR) {
-    // 0.01초 단위로 실행
-    var intervalID = setInterval(
-        function() {
-            // 버튼 이동
-            var left = parseInt(toggleBtn.css('left'));
-            left += (LR == 'TO_RIGHT') ? 5 : -5;
-            if(left >= 0 && left <= 40) {
-                left += 'px';
-                toggleBtn.css('left', left);
-            }
-        }, 10);
-    setTimeout(function(){
-        clearInterval(intervalID);
-    }, 201);
-}
-=======
 function makeArray(thisObj, len){
 	var element = thisObj.parent().children();
 	var dataAttr = new Array(); // 데이터 속성을 배열에 저장
@@ -535,4 +503,3 @@ function makeArray(thisObj, len){
 	}
 	
 }
->>>>>>> branch 'master' of https://github.com/kwang5348/TC_Generator.git
