66869da377cb331963ec6264015b55e3d500c478
Ramkwang <kwang5348@gmail.com>
2020-04-21 19:31:55 +0900

    버튼 꾸미기!

--- TcGenerator/WebContent/marutiadmin-master/js/main.js
+++ TcGenerator/WebContent/marutiadmin-master/js/main.js
@@ -30,7 +30,8 @@ Line.prototype.create = function(){
       <h5>라인</h5>
          </a> 
             <span class="icon2">
-               <button class="removeLineBtn">라인삭제</button>
+            	
+               	<button class="removeLineBtn"><i class="far fa-trash-alt"></i></button>
             </span>
             
 
@@ -364,4 +365,35 @@ function makeNumberString(thisObj, len){
 		}
 		OutputTag.val(OutputTag.val() + "\n")
 	}
+}
+
+$(document).on('click', '.toggleBG', function () {
+    var toggleBG = $(this);
+    var toggleFG = $(this).find('.toggleFG');
+    var left = toggleFG.css('left');
+    if(left == '40px') {
+        toggleBG.css('background', '#CCCCCC');
+        toggleActionStart(toggleFG, 'TO_LEFT');
+    }else if(left == '0px') {
+        toggleBG.css('background', '#53FF4C');
+        toggleActionStart(toggleFG, 'TO_RIGHT');
+    }
+});
+ 
+// 토글 버튼 이동 모션 함수
+function toggleActionStart(toggleBtn, LR) {
+    // 0.01초 단위로 실행
+    var intervalID = setInterval(
+        function() {
+            // 버튼 이동
+            var left = parseInt(toggleBtn.css('left'));
+            left += (LR == 'TO_RIGHT') ? 5 : -5;
+            if(left >= 0 && left <= 40) {
+                left += 'px';
+                toggleBtn.css('left', left);
+            }
+        }, 10);
+    setTimeout(function(){
+        clearInterval(intervalID);
+    }, 201);
 }
